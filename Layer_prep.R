d91d2502bbb178c71890ec4b03334c6c467c45c8
Claudia-Gutierrez <49790395+Claudia-Gutierrez@users.noreply.github.com>
2023-01-16 14:33:15 +0000

    subset heathland

--- Layer_prep.R
+++ Layer_prep.R
@@ -1,1 +1,16 @@
 library (sf)
+
+#Read original Priority Habitat Inventory England (phi) North
+N_phi<-st_read("spatial_data/Priority_Habitat_Inventory_England_1.shp")
+
+
+#subset heathland habitats
+table(N_phi$mainhabs) #habitat names
+N_heath_shp<- N_phi[N_phi$mainhabs=='Upland heathland'|N_phi$mainhabs=='Lowland heathland'|N_phi$mainhabs=='Fragmented heath'|N_phi$mainhabs=='Lowland dry acid grassland'|N_phi$mainhabs=='Lowland fens'|N_phi$mainhabs=='Upland flushes fens and swamps',]
+
+#Convert to raster with the characteristics of the England boundaries raster
+
+plot(N_heath_shp$mainhabs)
+
+
+  
