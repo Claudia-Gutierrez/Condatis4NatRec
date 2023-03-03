de0299da4237b4ab7b93a9f928c672fe612c0a21
Claudia-Gutierrez <49790395+Claudia-Gutierrez@users.noreply.github.com>
2023-03-03 17:06:43 +0000

    Bottlenecks area major and severe

--- scripts_methods/bottleneck_areas_function.R
+++ scripts_methods/bottleneck_areas_function.R
@@ -1,18 +1,23 @@
 
 library(sf)
 
+#inputs
+bottlenecks<-st_read('spatial_data/derived/national/heathland/3k/Nat_1k_heathland_3k_EW_bottlenecks.shp')
+score_major<-c(5,50) #score of bottlenecks considered as a priority
+score_severe<-50
 
-
-score_threshold<-50 #score of bottlenecks considered as a prioroty
-#read bottlenecks
-bottlenecks<- st_read('spatial_data/derived/local/Surrey/3k/Loc_250m_heathland_3k_EW_bottlenecks.shp')
 
 #assign British National Grid spetial reference
 bottlenecks<-st_transform(bottlenecks, st_crs('EPSG:27700'))
-st_crs(bottlenecks)
-plot(bottlenecks)
 
-mid_point<-st_line_sample(bottlenecks, sample=0.5)
-mid_point<-st_transform(mid_point, st_crs('EPSG:27700'))
+#subset major or severe bottlenecks 
+b_major<-bottlenecks[bottlenecks$score>score_major[1] & bottlenecks$score<score_major[2],]
+b_severe<-bottlenecks[bottlenecks$score>score_severe,]
+
+b_m_point<-st_line_sample(b_major, sample=0.5)
+b_m_point<-st_transform(b_m_point, st_crs('EPSG:27700'))
+b_m_point<-cbind(b_m_point,b_major)
+
+
 
 st_write(mid_point,'delete.shp', append = FALSE)
